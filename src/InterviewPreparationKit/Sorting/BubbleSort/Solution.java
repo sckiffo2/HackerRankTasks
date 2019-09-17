d86704f6bb8c7d44b4b98ae951d71e4e891062d4
Corvin <a.voronov.corvin@gmail.com>
2019-09-17 15:37:49 +0300

    bubble sort

---
+++ src/InterviewPreparationKit/Sorting/BubbleSort/Solution.java
@@ -0,0 +1,52 @@
+package InterviewPreparationKit.Sorting.BubbleSort;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+@SuppressWarnings("ALL")
+public class Solution {
+
+	// Complete the countSwaps function below.
+	static void countSwaps(int[] a) {
+		int swapCount = 0;
+		for (int i = a.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (a[j] > a[j + 1]) {
+					swapCount++;
+					int temp = a[j + 1];
+					a[j + 1] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.format("Array is sorted in %d swaps.%n", swapCount);
+		System.out.format("First Element: %d%n", a[0]);
+		System.out.format("Last Element: %d%n", a[a.length - 1]);
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		int[] a = new int[n];
+
+		String[] aItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int aItem = Integer.parseInt(aItems[i]);
+			a[i] = aItem;
+		}
+
+		countSwaps(a);
+
+		scanner.close();
+	}
+}
